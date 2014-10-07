d1c1c75192fc141dbb87bbff66fe35ae5550c616
Shumo Chu <shumo.chu@gmail.com>
2014-10-06 23:16:04 -0700

    add more boilerplates.

--- problemset1/partb/constants.hpp
+++ problemset1/partb/constants.hpp
@@ -9,6 +9,7 @@
 #define IP_FIELDS 4
 #define MAX_PORT 65535
 
-const static int BACK_LOG_SIZE = 16;
+const static int BACKLOG = 16;
 const static char * SERVER_IP = "127.0.0.1";
 const static char * SERVER_PORT = "9000";
+const static int MAX_CONNECTIONS = 10;
--- problemset1/partb/server.cc
+++ problemset1/partb/server.cc
@@ -8,18 +8,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-#include "server.hpp"
 #include <queue>
-#include "thread_pool.hpp"
 #include <sys/socket.h>
 #include <sys/types.h>
-#include "constants.hpp"
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <netdb.h>
+#include <iostream>
+#include <map>
+#include <unistd.h>
+#include <errno.h>
+
+#include "constants.hpp"
+#include "thread_pool.hpp"
+#include "server.hpp"
+
 
 using namespace std;
+
+
+// receive connections
+int receive_connections(const int socket_fd, int & request_id, 
+  map<int, pair<int, string> > & requests){
+  
+  struct sockaddr_storage their_addr;
+  socklen_t addr_size = sizeof their_addr;
+  
+  while(request_id<MAX_CONNECTIONS){
+    int new_fd = accept(socket_fd, (struct sockaddr *) & their_addr, &addr_size);
+  }
+  return 0;
+}
+
 
 //main thread execution call
 int initialize_server(const char * ip_address, const char * port)
@@ -27,12 +49,14 @@ int initialize_server(const char * ip_address, const char * port)
   
   //boilerplates
   int status;
-  struct addrinfo hints, *res, *p;
+  struct addrinfo hints, *servinfo, *p;
   socklen_t addr_size;
   struct sockaddr_in socket_addr;
+  int yes = 1;
+  int server_socket = -1;
 
   //Initialize the thread pool
-  initialize_thread_pool(THREAD_POOL_SIZE);
+  //initialize_thread_pool(THREAD_POOL_SIZE);
   
   //Open a TCP listening connection
   // 1 - load up address structs
@@ -41,31 +65,62 @@ int initialize_server(const char * ip_address, const char * port)
   // 4 - listen for connections
   // 5 - accept connections
 
-  // load up address structs
+  //load up address structs
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;        // IPv4, we are old fashioned
   hints.ai_socktype = SOCK_STREAM;  // TCP
   hints.ai_flags = AI_PASSIVE;      // fill in my IP for me 
 
-  if((status = getaddrinfo(NULL, port, &hints, &res))!=0){
+  if((status = getaddrinfo(NULL, port, &hints, &servinfo))!=0){
     fprintf(stderr, "getaddrinfo: %s \n", gai_strerror(status));
     exit(EXIT_FAILURE);
   }
 
-  // create a listening socket
-  int server_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-  if (server_socket == -1){
-    perror("Cannot create socket, aborted.");
+  // loop through all the results and bind to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next){
+    // try to create a listening port
+    if((server_socket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1){
+      perror("[WARN] error at server:socket, try again.\n");
+      continue;
+    }
+
+    // set socket option
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
+      perror("[ERROR] error at setsocket, abort. \n");
+      exit(EXIT_FAILURE);
+    }
+
+    //bind the socket
+    if(bind(server_socket, p->ai_addr, p->ai_addrlen) == -1){
+      close(server_socket);
+      perror("[WARN] error at binding, try again.");
+      continue;
+    }
+
+    break;
+  }
+
+  if (p == NULL){
+    perror("[ERROR] server: fail to bind. \n");
     exit(EXIT_FAILURE);
   }
 
-  // bind socket
-  if(bind(server_socket, res->ai_addr, res->ai_addrlen) == -1){
-    perror("Cannot bind socket.");
+  freeaddrinfo(servinfo);
+
+  //listening 
+  if(listen(server_socket, BACKLOG) == -1){
+    perror("[ERROR] Fail to listen, aborted.");
     exit(EXIT_FAILURE);
+  } else {
+    fprintf(stdout, "[INFO] begin to listen on %d \n", server_socket);
   }
 
-  //Loop for connections until server is terminated
+  //load static resources
+  //TODO
+
+  //event based connection handling
+  int request_id = 0;                    //unique identifier for each request
+  map<int, pair<int, string> > requests; //requests, key=request id, v=sock,url
 
   //Validate filepath request
 
@@ -76,10 +131,7 @@ int initialize_server(const char * ip_address, const char * port)
   //If running threads fail to terminate, kill them
 
   //Destroy the thread pool
-  destroy_thread_pool();
-
-  //Free memory
-  freeaddrinfo(res); 
+  //destroy_thread_pool();
 
   return 0;
 }
