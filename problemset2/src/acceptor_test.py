929a536c0aec10d73dc1279e82c9e2c6d4119a85
agent-lee <vlee2@cs.washington.edu>
2014-10-18 22:38:30 -0700

    Acceptor test refactor.

--- problemset2/src/acceptor_test.py
+++ problemset2/src/acceptor_test.py
@@ -17,10 +17,8 @@ import message
 from multiprocessing import Queue, Process, Lock
 
 class acceptor_test(unittest.TestCase):
-    
 
-    # Tests the acceptor in isolation
-    def test_acceptor(self):
+    def setUp(self):
 
         # Remote server message receive socket = 9003
         # Acceptor message receive socket = 9001
@@ -29,7 +27,7 @@ class acceptor_test(unittest.TestCase):
         print "[Info] Initializing acceptor test with PID " + str(os.getpid())
 
         # Instantiate a server instance
-        paxos_server = server.server('localhost', 9000, 0, 2)
+        self.paxos_server = server.server('localhost', 9000, 0, 2)
 
         # Insert a wait for the server to come online
         time.sleep(1)
@@ -38,59 +36,69 @@ class acceptor_test(unittest.TestCase):
 
         # start a test remote inter-server socket on 9003
         try:
-            test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            test_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            test_socket.bind(('localhost', 9003))
-            test_socket.listen(5)
+            self.test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.test_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.test_socket.bind(('localhost', 9003))
+            self.test_socket.listen(5)
         except Exception, e:
             os.system("lsof -n -i")
             os.system("ps -a")
             print "[Info] Failed to bind socket for dummy server: " + str(e)
-            test_socket.close()
+            self.test_socket.close()
             
             assert(False)
 
         # populate the server list
-        server_list = [('localhost', 9003)]
+        self.server_list = [('localhost', 9003)]
 
         print "[Info] Bound a dummy server port for the acceptor to connect to..."
 
         # initialize the acceptor which should initiate a connection to 9003
-        acceptor_process = paxos_server.launch_acceptor_process('localhost', 9001, 0, 2, server_list)
+        self.acceptor_process = self.paxos_server.launch_acceptor_process('localhost', 9001, 0, 2, self.server_list)
 
         print "[Info] Initialized an acceptor instance.."
 
         # create a test socket to inject messages to the acceptor and connect to 9001
-        message_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        test_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        message_socket.connect(('localhost', 9001))
+        self.message_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.message_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.message_socket.connect(('localhost', 9001))
 
         print "[Info] Connected a dummy server message channel to Paxos server..."
 
         # accept the incoming connection that should have been made from 9001 to 9003
-        (acceptor_connection, acceptor_address) = test_socket.accept()
+        (self.acceptor_connection, acceptor_address) = self.test_socket.accept()
 
         print "[Info] Received test server connection to dummy server.."
         
+
+    ###########################################################
+    # Test acceptor for graceful bring up and exit
+    ###########################################################
+
+    def test_exit(self):
+
         # shut down the acceptor by sending an exit message from 9003 to 9001
         msg = message.message(message.MESSAGE_TYPE.EXIT,
                               None, None, None, 'localhost', 9003, None)
-        message_socket.send(pickle.dumps(msg))
+        self.message_socket.send(pickle.dumps(msg))
 
         print "[Info] Issued a shutdown message..."
 
+    def tearDown(self):
+
         # clean up the sockets
-        message_socket.close()
-        test_socket.close()
+        self.message_socket.close()
+        self.test_socket.close()
+        self.acceptor_connection.close()
 
         # attempt to join the processes
         try:
-            acceptor_process.join(1)
+            self.acceptor_process.join(1)
         except Exception, e:
             assert(False)
 
         # terminate the connection process
-        paxos_server.listening_process.terminate()
+        self.paxos_server.listening_process.terminate()
 
 if __name__ == '__main__':
     unittest.main()
