18faa146e2d352038cef556861406005c4c52d01
Shumo Chu <shumo.chu@gmail.com>
2014-11-13 11:04:37 -0800

    big fix

--- problemset3/src/lca.py
+++ problemset3/src/lca.py
@@ -4,12 +4,12 @@
 
 from pyspark import SparkContext
 
-print "\n\n################[BEGINNING SPARK APPLICATION]######################\n\n"
+print "\n################[BEGINNING SPARK APPLICATION]#####################\n"
 
 sc = SparkContext("local", "LCA_APPLICATION")
 
 cites_bucket="s3n://AKIAI6XU3D7NLMMQ5DMQ:rT3dXyT+2U4MoYRDa1qmCnWQXrmX+czTgZMLxuPw@550.cs.washington.edu/cites.csv"
-cites_bucket="s3n://AKIAI6XU3D7NLMMQ5DMQ:rT3dXyT+2U4MoYRDa1qmCnWQXrmX+czTgZMLxuPw@550.cs.washington.edu/papers.csv"
+papers_bucket="s3n://AKIAI6XU3D7NLMMQ5DMQ:rT3dXyT+2U4MoYRDa1qmCnWQXrmX+czTgZMLxuPw@550.cs.washington.edu/papers.csv"
 
 cites_data = sc.textFile(cites_bucket)
 papers_data = sc.textFile(papers_bucket)
@@ -19,5 +19,5 @@ print "paper count ：{}".format(papers_data.count())
 #STUB ADD APPLICATION LOGIC HERE
 
 
-print "\n\n################[TERMINATING SPARK APPLICATION]######################\n\n"
+print "\n################[TERMINATING SPARK APPLICATION]####################\n"
 
