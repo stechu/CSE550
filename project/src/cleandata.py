57e40a469f58e17c51d4239e07f79010f4aedb4c
Shumo Chu <shumo.chu@gmail.com>
2014-12-03 00:01:03 -0800

    special boolean column

--- project/src/cleandata.py
+++ project/src/cleandata.py
@@ -12,11 +12,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("folder", help="folder that stores input csvs.")
 parser.add_argument("output_file", help="output file name.")
 parser.add_argument(
+    "default_zero_col", help="the column need null default to zero", type=int)
+parser.add_argument(
     "num_cols", help="number of columns of input csvs.", type=int)
 args = parser.parse_args()
 
 
-def clean_data(folder, output_name, num_cols):
+def clean_data(folder, output_name, zero_col, num_cols):
     # create writer
     output_file = open(output_name, "wb")
     writer = csv.writer(output_file)
@@ -31,11 +33,12 @@ def clean_data(folder, output_name, num_cols):
                     write_line = line
                     for idx, cell in enumerate(write_line):
                         if cell == "":
-                            write_line[idx] = -1
+                            write_line[idx] = -1 if idx != zero_col else 0
                     # sanity checking
                     assert len(write_line) == num_cols
                     # do the write
                     writer.writerow(write_line)
 
 if __name__ == "__main__":
-    clean_data(args.folder, args.output_file, args.num_cols)
+    clean_data(
+        args.folder, args.output_file, args.default_zero_col, args.num_cols)
